59fd4eb47c7d67390e57cff9ed367493b0d2d164
enesb <enesb@DESKTOP-U5KNAQN.mynetworksettings.com>
2023-05-27 22:36:30 -0400

    added comment

--- LearningGit/src/test/java/com/practice/git/HelloWorld.java
+++ LearningGit/src/test/java/com/practice/git/HelloWorld.java
@@ -2,4 +2,14 @@ package com.practice.git;
 
 public class HelloWorld {
 
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	//11111
+	public void emtyMethod() {
+		
+	}
+	public void emtyhy2() { 
+		
+	}
 }
